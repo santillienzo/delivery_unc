876d8eb57a741c1d78d6244756f46a30e082a925
santillienzo <enzo135246@gmail.com>
2022-03-25 00:06:23 -0300

    refactor: funcionalidad del añadir y eliminar productos al carrito.

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,14 +1,19 @@
+// Atrapo el ícono del carrito y el carrito en sí
 const carrito_icono = document.getElementById('carrito_icon')
 const carrito = document.getElementById('carrito__container')
 
-let carrito_activado = false
+// Variable bandera para controlar que el carrito este visible o no.
+let carrito_visible = false
 
+//Al hacer click en en el ícono del carrito:
 carrito_icono.addEventListener("click", ()=>{
-    if (carrito_activado) {
+    //Si el carrito es visible le vamos a dar un opacity de 0 al carrito y la variable bandera se volverá falsa
+    if (carrito_visible) {
         carrito.style.opacity = "0"
-        carrito_activado = false
+        carrito_visible = false
     }else{
+    //Si el carrito no es visible le vamos a dar un opacity de 1 al carrito y la variable bandera se volverá verdadera
         carrito.style.opacity = "1"
-        carrito_activado = true
+        carrito_visible = true
     }
 })
--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,4 @@
-window.onload = () => {
-
-  let carrito = {};
-
+window.onload = async () => {
   //----------------ZONA DE DEFINICION DE FUNCIONES ---------------
   function listarTodo() {
     let seccionHamburguesa = document.getElementById("seccionHamb");
@@ -17,22 +14,15 @@ window.onload = () => {
           articuloEnHtml = `
             <article class="articulo">
               <img class="card-foto" src="${producto.imagen}" alt="" />
-              <input 
-                type="hidden" 
-                class = "propiedadId"
-                name="idPropiedad"
-                id="idPropiedad" 
-                value="${producto.id}"
-              />
-              <h3>${producto.nombre}</h3>
-              <h2>${producto.precio}</h2>
-              <p> ${producto.descripcion}</p>
+              <h3 class="name">${producto.nombre}</h3>
+              <h2 class="price">${producto.precio}</h2>
+              <p class="description"> ${producto.descripcion}</p>
               <div class="footer-card">
                 <a 
                   class="vinculo" 
                   href="propiedadesDetail.html?zona=${producto.zona}&descripcion=${producto.Descripcion}"
                 >Ver Más<i class="fas fa-angle-double-right"></i></a>
-                  <i class="fas fa-shopping-cart carrito-logo"  ></i>
+                  <i class="fas fa-shopping-cart añadir_carrito" id-item=${producto.id}></i>
                   <i id="corazon" class="fas fa-heart"></i>
               </div>
             </article>
@@ -44,86 +34,140 @@ window.onload = () => {
           } else {
             seccionPizza.innerHTML += articuloEnHtml;
           }
-
-          const carritoIcono = document.querySelectorAll(".carrito-logo");
-
-          carritoIcono.forEach((btn) => {
-            btn.addEventListener("click", (e) => agregarAlCarrito(e));
-          });
         });
+
+        cargarEscuchaDeEventos()
       }) // Aquí mostramos dicha información
       .catch((error) => console.log("Hubo un error : " + error.message));
   }
 
-  function agregarAlCarrito(e) {
-    // console.log(e.target);
-    if (e.target.classList.contains("carrito-logo")) {
-      unItemAlCarrito(e.target.parentElement.parentElement);
-    }
-
-    e.stopPropagation();
-  }
-
-  function unItemAlCarrito(objeto) {
-    console.log(objeto);
-    console.log("AGrego un item");
-    const producto = {
-      id: objeto.querySelector(".propiedadId").value,
-      precio: objeto.querySelector("h2").textContent,
-      nombre: objeto.querySelector("h3").textContent,
-      descripcion: objeto.querySelector("p").textContent,
-      cantidad: 1,
-    };
-    if (carrito.hasOwnProperty(producto.id)) {
-      producto.cantidad = carrito[producto.id].cantidad + 1;
-    }
-
-    carrito[producto.id] = { ...producto };
-
-    localStorage.setItem("carritoClave", JSON.stringify(carrito));
-
-    renderCarrito();
-  }
-
-  // -------DEFINICION DE LA FUNCION RENDER CARRITO
-  function renderCarrito() {
-    console.log("El contenido del carrito");
-    console.log(carrito);
-    console.log(typeof(carrito)) 
-    // alert("carrito fuera del map");
-
-    tbody.innerHTML = ' ';
-    //  Los objetos deben transformarse con Object.values
-    Object.values(carrito).forEach(item =>{
-      const tr = document.createElement("tr");
-
-      const linea = `
-      <tr>
-        <td class="table-zona">${item.nombre}</td>
-        <td class="table-precio">$${item.precio}</td>
-        <td class="table-cantidad">
-        <input type="number" min = "1" value=${item.cantidad} name="" id="">
-        </td> 
-        <button class="eliminar-item"> Eliminar</button>
-      </tr>
-      `;
-      tr.innerHTML = linea;
-      tbody.append(tr);
+  //En esta función guardamos todos los eventos que utilicemos 
+  function cargarEscuchaDeEventos() {
+    //Atrapamos todos los botones para añadir al carrito
+    const add_cart_buttons = document.querySelectorAll('.añadir_carrito')
+    
+    //Al hacer click en uno de los botones ejecutamos lo siguiente:
+    add_cart_buttons.forEach((btn)=>{
+      btn.addEventListener("click", (e)=> {
+        añadirCarrito(e.target.parentElement.parentElement)
+      })
     })
+
+    //Al hacer click en la tabla del carrito ejecutamos la función eliminarProducto()
+    tbody.addEventListener("click", e => eliminarProducto(e))
   }
 
-  //----DEFINICION DE LA FUNCION DE LOCAL STORAGE
-  function addLocalStorage() {
-    localStorage.setItem("carritoClave", JSON.stringify(carrito));
-  }
+  function eliminarProducto(e) {
+    //Si hacemos click sobre el btn de eliminar
+    if (e.target.classList.contains('eliminar-item')) {
+      //Obtenemos la id del producto con el atributo 'data-id' asignado al boton
+      const deleteId = e.target.getAttribute('data-id')
 
-  let tbody = document.getElementById("tbody");
-  // atrapo el tbody
+      //Recorremos los productos del carrito
+      carrito.forEach(producto =>{
+        //Si el producto donde estamos posicionados es igual al deleteId:
+        if (producto.id === deleteId) {
+          //Definimos una variable que es igual al precio del producto que eliminamos por la cantidad que hay
+          let priceReduce = parseFloat(producto.precio) * parseFloat(producto.cantidad);
+          _priceTotal = _priceTotal - priceReduce //Restamos el precio total
+          _priceTotal = _priceTotal.toFixed(2)
+          _countCart = _countCart - producto.cantidad //Reducimos el contador
+        }
+      })
+      carrito = carrito.filter(producto=> producto.id !== deleteId)
+      localStorage.setItem("carritoClave", JSON.stringify(carrito));
+    }
 
-  if (localStorage.getItem("carritoClave")) {
-    carrito = JSON.parse(localStorage.getItem("carritoClave"));
     renderCarrito()
   }
 
+
+  function añadirCarrito(productoSeleccionado){
+    //Extraemos todos los datos del producto que seleccionemos
+    const item = {
+      id: productoSeleccionado.querySelector('.añadir_carrito').getAttribute('id-item'),
+      nombre: productoSeleccionado.querySelector('.name').textContent,
+      descripcion: productoSeleccionado.querySelector('.description').textContent,
+      precio: productoSeleccionado.querySelector('.price').textContent,
+      cantidad: 1
+    }
+
+    //Sumamos el precio total
+    _priceTotal = parseFloat(_priceTotal) + parseFloat(item.precio)
+    _priceTotal = _priceTotal.toFixed(2)
+
+    //Comprobamos que el producto existe en el carrito
+    const exist = carrito.some(producto => producto.id === item.id);
+
+    //En caso de que así sea sumamos 1 a la cantidad del producto, y si no es así agregamos el producto al render
+    if (exist) {
+      const pro = carrito.map(producto=>{
+        if (producto.id === item.id) {
+          producto.cantidad++
+          _countCart++;
+          return producto
+        }
+      })
+      carrito = [...pro]
+    }else{
+      carrito = [...carrito, item]
+      _countCart++;
+    }
+
+    localStorage.setItem("carritoClave", JSON.stringify(carrito));
+
+    renderCarrito()
+    console.log(item)
+  }
+
+  //Renderizamos el carrito
+  function renderCarrito() {
+    tbody.innerHTML=' '
+
+    carrito.forEach(producto=>{
+      const tr = document.createElement("tr");
+      const {id, nombre, descripcion, precio, cantidad} = producto;
+      const fila = `
+        <td class="table-zona">${nombre}</td>
+        <td class="table-precio">$${precio}</td>
+        <td class="table-cantidad">
+          <input type="number" min = "1" value=${cantidad} name="" id="">
+        </td> 
+        <button class="eliminar-item" data-id=${id}> Eliminar</button>
+      `;
+      tr.innerHTML = fila;
+      tbody.append(tr);
+    })
+
+    priceTotal.innerHTML = _priceTotal
+    countCart.innerHTML = _countCart
+  }
+
+
+  //Atrapamos los elementos del html
+  const tbody = document.getElementById("tbody"); //tbody
+  const priceTotal = document.querySelector('.itemCartTotal span') //Precio total del carritos
+  const countCart = document.querySelector('.carrito_contador') //contador de productos
+
+  //Definimos las variables
+  let carrito = []; //Array donde se guardarán los productos del carrito
+  let _priceTotal = 0; //Precio total
+  let _countCart = 0;//Contador de productos
+  //Si existe el carrito en el LocalStorage: 
+  if (localStorage.getItem("carritoClave")) {
+    //Nuestra variable carrito será igual a lo guardado en el local storage
+    carrito = JSON.parse(localStorage.getItem("carritoClave"));
+    //Recorremos el carrito
+    carrito.map(product=>{
+      //El precio total es igual al valor de precio total + precio del producto multiplicadp por la cantidad
+      _priceTotal = _priceTotal + product.precio * product.cantidad
+      //El contador es igual al valor de contador + la cantidad del producto
+      _countCart = _countCart + product.cantidad
+    })
+    
+    renderCarrito()
+  }
+  
   listarTodo();
+  
 };
