f6e001e845eb7bb57aa1689b08234d6c23a14da0
santillienzo <enzo135246@gmail.com>
2022-04-02 16:56:53 -0300

    fix: add products from search

--- public/js/pages/search.js
+++ public/js/pages/search.js
@@ -1,4 +1,5 @@
 import {renderBusqueda} from '../render/renderBusqueda.js'
+import { agregarProducto } from '../utils/cart.js';
 
 //window.location.search sirve para ver los pará,etros que trae consigo la url
 const valores = window.location.search;
@@ -12,4 +13,15 @@ const textField = document.querySelector('.input__search')
 
 textField.value = busqueda
 
-renderBusqueda(id_categoria, busqueda)
+function cargarEscuchaDeEventos() {
+    //Atrapamos todos los botones para añadir al carrito
+    const add_cart_buttons = document.querySelectorAll('.agregar_carrito')
+    
+    //Al hacer click en uno de los botones ejecutamos lo siguiente:
+    add_cart_buttons.forEach((btn)=>{
+        btn.addEventListener("click", e => agregarProducto(e.target.parentElement.parentElement))
+    })
+}
+
+await renderBusqueda(id_categoria, busqueda)
+cargarEscuchaDeEventos()
